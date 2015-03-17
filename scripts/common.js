25f333b7b541c4ee1f18d1e304aadbd89ccc1bc9
Verycoder <626897681@qq.com>
2015-03-18 02:16:32 +0800

    Add method for get content from other document

--- scripts/common.js
+++ scripts/common.js
@@ -9,16 +9,30 @@ function showChile(input){
 	}
 }
 
-function openDetail(input){
+function openDetail(input, contentUrl, musicUrl){
 	var detail = $(input).parent().parent().parent().find("div");
+	if(contentUrl != null){
+		var content = $.ajax({url:contentUrl, async:false});
+		detail.html(content.responseText);
+	}
+	if(musicUrl != null){
+		$("EMBED").attr("hidden","hidden");
+		var newEmbed = '<EMBED src="'+ musicUrl +'" width=0 height=0 volume=70 autostart=true loop=5 style="background:white"></EMBED>';
+		$("#music").html(newEmbed);
+	}
+	contentUrl = "'"+ contentUrl +"'";
+	musicUrl = "'"+ musicUrl +"'";
 	detail.removeAttr("hidden");
 	$(input).html("收起");
-	$(input).attr("onclick","closeDetail(this)");
+	$(input).attr("onclick","closeDetail(this, "+ contentUrl +", " + musicUrl + ")");
+	
 }
 
-function closeDetail(input){
+function closeDetail(input, contentUrl, musicUrl){
+	contentUrl = "'"+ contentUrl +"'";
+	musicUrl = "'"+ musicUrl +"'";
 	var detail = $(input).parent().parent().parent().find("div");
 	detail.prop("hidden","hidden");
 	$(input).html("展开");
-	$(input).attr("onclick","openDetail(this)");
+	$(input).attr("onclick","openDetail(this, "+ contentUrl +", " + musicUrl + ")");
 }
